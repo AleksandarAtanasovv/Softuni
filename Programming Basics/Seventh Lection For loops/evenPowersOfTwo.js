bc5abc092454c971a63b256770009a7fc2f348b1
Sakinaa782 <sashotrupa@hotmail.com>
2022-06-25 12:15:30 +0300

    More Exercises

---
+++ Programming Basics/Seventh Lection For loops/evenPowersOfTwo.js
@@ -0,0 +1,10 @@
+function evenPowersOfTwo(input){
+    let n = Number(input[0]);
+    let sum = 1;
+
+    for(let i = 0; i <= n; i+=2){
+        console.log(sum);
+        sum = sum * 2 * 2;
+    }
+}
+evenPowersOfTwo(["7"])
