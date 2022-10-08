0c2e3e1af8f4f8443391fcdd12fec178d0ce29aa
Sakinaa782 <sashotrupa@hotmail.com>
2022-10-08 10:07:02 +0300

    More Exercises Added

---
+++ Programming Fundamentals/Functions - More Exercise/pointsValidation.js
@@ -0,0 +1,32 @@
+function pointsValidation(arrayOfCoords) {
+  const [x1, y1, x2, y2] = arrayOfCoords;
+  let checkFirstPoint = FirstPoint();
+  let checkSecondPoint = SecondPoint();
+  let checkThirdPoint = ThirdPoint();
+  if (checkFirstPoint === Math.trunc(checkFirstPoint)) {
+    console.log(`{${x1}, ${y1}} to {0, 0} is valid`);
+  } else {
+    console.log(`{${x1}, ${y1}} to {0, 0} is invalid`);
+  }
+  if (checkSecondPoint === Math.trunc(checkSecondPoint)) {
+    console.log(`{${x2}, ${y2}} to {0, 0} is valid`);
+  } else {
+    console.log(`{${x2}, ${y2}} to {0, 0} is invalid`);
+  }
+  if (checkThirdPoint === Math.trunc(checkThirdPoint)) {
+    console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is valid`);
+  } else {
+    console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is invalid`);
+  }
+
+  function FirstPoint() {
+    return Math.sqrt(Math.pow(x1, 2) + Math.pow(y1, 2));
+  }
+  function SecondPoint() {
+    return Math.sqrt(Math.pow(x2, 2) + Math.pow(y2, 2));
+  }
+  function ThirdPoint() {
+    return Math.sqrt(Math.pow(Math.abs(x2 - x1), 2) + Math.pow(Math.abs(y2 - y1), 2));
+  }
+}
+pointsValidation([2, 1, 1, 1]);
