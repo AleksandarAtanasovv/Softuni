49b2e2ffdc3453142b5da98dbc4109f2a6540b8d
Sakinaa782 <sashotrupa@hotmail.com>
2023-01-20 23:00:48 +0200

    More Exercises Added

--- JS Advanced/JS Advanced/DOM Introduction - Lab/05.Colorize Table/ColorizeTable.js
+++ JS Advanced/JS Advanced/DOM Introduction - Lab/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,8 @@
 function colorize() {
-    // TODO
+  const items = document.querySelectorAll("tr");
+  Array.from(items).forEach((x, i) => {
+    if (i % 2 !== 0) {
+      x.style.backgroundColor = "Teal";
+    }
+  });
 }
