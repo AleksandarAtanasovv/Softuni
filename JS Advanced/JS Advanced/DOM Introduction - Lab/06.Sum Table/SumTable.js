f63895a54976d3440e8d006067f36fa8adf098fc
Sakinaa782 <sashotrupa@hotmail.com>
2023-01-20 23:40:03 +0200

    More Exercises Added

--- JS Advanced/JS Advanced/DOM Introduction - Lab/06.Sum Table/SumTable.js
+++ JS Advanced/JS Advanced/DOM Introduction - Lab/06.Sum Table/SumTable.js
@@ -1,3 +1,11 @@
 function sumTable() {
-
+  const table = document.querySelectorAll("table td");
+  const result = document.getElementById("sum");
+  let sum = 0;
+  Array.from(table).forEach((element) => {
+    if (!isNaN(Number(element.textContent))) {
+      sum += Number(element.textContent);
+    }
+  });
+  result.textContent = sum;
 }
--- JS Advanced/JS Advanced/DOM Introduction - Lab/07.Extract Parenthesis/ExtractParenthesis.js
+++ JS Advanced/JS Advanced/DOM Introduction - Lab/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,5 @@
 function extract(content) {
-
+  const element = document.getElementById(content).textContent;
+  const res = element.trim().match(/(?!\()[\w*\s*\d*]*(?=\))/g);
+  return res.filter((x) => x !== "").join("; ");
 }
