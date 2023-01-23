ae59dd8caf6c134683a123a70ff2cb3f4dcf7166
Sakinaa782 <sashotrupa@hotmail.com>
2023-01-23 23:43:55 +0200

    More Exercises Added

--- JS Advanced/JS Advanced/DOM Introduction - Exercise/Format the Text/solution.js
+++ JS Advanced/JS Advanced/DOM Introduction - Exercise/Format the Text/solution.js
@@ -1,3 +1,16 @@
 function solve() {
-  //TODO
+  const textarea = document.querySelector("#exercise>textarea");
+  const output = document.getElementById("output");
+  const res = textarea.value.split(".").filter((x) => x.length > 0);
+
+  for (let i = 0; i < res.length; i += 3) {
+    let arr = [];
+    for (let j = 0; j < 3; j++) {
+      if (res[i + j]) {
+        arr.push(res[i + j]);
+      }
+    }
+    const p = arr.join(". ") + ".";
+    output.innerHTML += `<p>${p}</p>`;
+  }
 }
--- JS Advanced/JS Advanced/DOM Introduction - Exercise/Table - Search Engine/solution.js
+++ JS Advanced/JS Advanced/DOM Introduction - Exercise/Table - Search Engine/solution.js
@@ -1,8 +1,18 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
-
-   function onClick() {
-      //   TODO:
-
-   }
+  document.querySelector("#searchBtn").addEventListener("click", onClick);
+  const result = document.getElementById("result");
+  const searchField = document.getElementById("searchField");
+  const trs = Array.from(document.querySelectorAll("tbody tr"));
+  function onClick() {
+    if (searchField.value.length > 0) {
+      trs.forEach((element) => {
+        if (element.textContent.toLowerCase().includes(searchField.value.toLowerCase())) {
+          element.classList.add("select");
+        } else {
+          element.classList.remove("select");
+        }
+      });
+    }
+    searchField.value = "";
+  }
 }
