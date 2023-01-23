bbdc0cd4015e4fbea341718de32359480c0e68c0
Sakinaa782 <sashotrupa@hotmail.com>
2023-01-23 23:04:23 +0200

    More Exercises Added

--- JS Advanced/JS Advanced/DOM Introduction - Exercise/Accordion/accordion.js
+++ JS Advanced/JS Advanced/DOM Introduction - Exercise/Accordion/accordion.js
@@ -1,3 +1,11 @@
 function toggle() {
-    console.log('TODO:...');
+  const button = document.querySelector(".button");
+  const extra = document.getElementById("extra");
+  if (button.textContent === "More") {
+    button.textContent = "Less";
+    extra.style.display = "block";
+  } else {
+    button.textContent = "More";
+    extra.style.display = "none";
+  }
 }
--- JS Advanced/JS Advanced/DOM Introduction - Exercise/Search in List/search.js
+++ JS Advanced/JS Advanced/DOM Introduction - Exercise/Search in List/search.js
@@ -1,3 +1,21 @@
 function search() {
-   // TODO
+  const towns = Array.from(document.querySelectorAll("#towns li"));
+  const searchText = document.getElementById("searchText");
+  const result = document.getElementById("result");
+  let counter = 0;
+
+  if (searchText.value.length > 0) {
+    towns.forEach((town) => {
+      if (town.textContent.toLowerCase().includes(searchText.value.toLowerCase())) {
+        town.style.fontWeight = "bold";
+        town.style.textDecoration = "underline";
+        counter++;
+      } else {
+        town.style.fontWeight = "normal";
+        town.style.textDecoration = "none";
+      }
+    });
+  }
+  searchText.value = "";
+  result.textContent = `${counter} matches found`;
 }
