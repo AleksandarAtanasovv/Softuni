023cde948cb4ff954f940d43fea1e3f350773f2e
Sakinaa782 <sashotrupa@hotmail.com>
2023-01-27 22:39:54 +0200

    More Exercises Added

---
+++ JS Advanced/JS Advanced/Advanced Functions - Lab/filterEmployees.js
@@ -0,0 +1,33 @@
+function filterEmployees(inputJSON, criteria) {
+  const input = JSON.parse(inputJSON);
+  const [criteriaType, crieriaValue] = criteria.split("-");
+  return input
+    .filter((x) => x[criteriaType] === crieriaValue)
+    .map((x, i) => `${i}. ${x.first_name} ${x.last_name} - ${x.email}`)
+    .join("\n");
+}
+console.log(
+  filterEmployees(
+    `[{
+      "id": "1",
+      "first_name": "Ardine",
+      "last_name": "Bassam",
+      "email": "abassam0@cnn.com",
+      "gender": "Female"
+    }, {
+      "id": "2",
+      "first_name": "Kizzee",
+      "last_name": "Jost",
+      "email": "kjost1@forbes.com",
+      "gender": "Female"
+    },  
+  {
+      "id": "3",
+      "first_name": "Evanne",
+      "last_name": "Maldin",
+      "email": "emaldin2@hostgator.com",
+      "gender": "Male"
+    }]`,
+    "gender-Female"
+  )
+);
