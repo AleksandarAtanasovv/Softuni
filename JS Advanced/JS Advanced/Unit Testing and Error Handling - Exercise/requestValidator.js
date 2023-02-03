7c9330d01bef8b9754e43a296d830c57239862ca
Sakinaa782 <sashotrupa@hotmail.com>
2023-02-03 13:12:38 +0200

    More Exercises Added

--- JS Advanced/JS Advanced/Unit Testing and Error Handling - Exercise/requestValidator.js
+++ JS Advanced/JS Advanced/Unit Testing and Error Handling - Exercise/requestValidator.js
@@ -1,11 +1,14 @@
 function requestValidator(req) {
   const methods = ["GET", "POST", "DELETE", "CONNECT"];
   const versions = ["HTTP/0.9", "HTTP/1.0", "HTTP/1.1", "HTTP/2.0"];
-  const messagePattern = /^([^<>\\&'"]*)$/g;
-  const uriPattern = /^([a-zA-Z0-9\.]+|\*)$/gm;
+  const messagePattern = /^[^<>\\&'"]*$/g;
+  const uriPattern = /^(\.*[a-zA-Z]*[0-9]*\.*\**)+$/g;
   const { method, uri, version, message } = req;
   if (!methods.includes(method)) {
     throw new Error("Invalid request header: Invalid Method");
+  }
+  if (!uri || !uriPattern.test(uri)) {
+    throw new Error("Invalid request header: Invalid URI");
   }
   if (!versions.includes(version)) {
     throw new Error("Invalid request header: Invalid Version");
@@ -13,9 +16,7 @@ function requestValidator(req) {
   if (message === undefined || !messagePattern.test(message)) {
     throw new Error("Invalid request header: Invalid Message");
   }
-  if (!uri || !uriPattern.test(uri)) {
-    throw new Error("Invalid request header: Invalid URI");
-  }
+
   return req;
 }
 console.log(
