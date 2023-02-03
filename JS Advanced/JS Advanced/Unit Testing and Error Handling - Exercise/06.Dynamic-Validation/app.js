dd193504d58b1ec6362d235d1775534e19c13f2c
Sakinaa782 <sashotrupa@hotmail.com>
2023-02-03 11:56:24 +0200

    More Exercises Added

--- JS Advanced/JS Advanced/Unit Testing and Error Handling - Exercise/06.Dynamic-Validation/app.js
+++ JS Advanced/JS Advanced/Unit Testing and Error Handling - Exercise/06.Dynamic-Validation/app.js
@@ -1,3 +1,11 @@
 function validate() {
-  // TODO
+  const emailField = document.getElementById("email");
+  const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+  emailField.addEventListener("change", (e) => {
+    if (!emailRegex.test(e.target.value)) {
+      emailField.classList.add("error");
+    } else {
+      emailField.classList.remove("error");
+    }
+  });
 }
