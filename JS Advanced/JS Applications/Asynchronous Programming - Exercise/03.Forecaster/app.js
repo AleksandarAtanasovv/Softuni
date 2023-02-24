1d9cca08e3e553cbe87d95ec8610f919bc3ceaf9
Sakinaa782 <sashotrupa@hotmail.com>
2023-02-24 23:33:20 +0200

    More Exercises Added

--- JS Advanced/JS Applications/Asynchronous Programming - Exercise/03.Forecaster/app.js
+++ JS Advanced/JS Applications/Asynchronous Programming - Exercise/03.Forecaster/app.js
@@ -1,5 +1,79 @@
 function attachEvents() {
-    console.log("TODO...");
+  const submit = document.getElementById("submit");
+  submit.addEventListener("click", getWeather);
+
+  async function getWeather() {
+    const location = document.getElementById("location").value;
+    const responseLocations = await fetch("http://localhost:3030/jsonstore/forecaster/locations");
+    const dataLocations = await responseLocations.json();
+
+    const town = dataLocations.filter((t) => t.name == location)[0];
+    document.getElementById("forecast").style.display = "block";
+    try {
+      const code = town.code;
+      const cityCurrentDayResponse = await fetch(
+        `http://localhost:3030/jsonstore/forecaster/today/${code}`
+      );
+      const cityThreeDayResponse = await fetch(
+        `http://localhost:3030/jsonstore/forecaster/upcoming/${code}`
+      );
+
+      const dataCurrentDay = await cityCurrentDayResponse.json();
+      const cityThreeDayData = await cityThreeDayResponse.json();
+      displayCurrentDay(dataCurrentDay);
+      displayThreeDay(cityThreeDayData);
+    } catch (err) {
+      document.getElementById("forecast").textContent = "Error";
+    }
+  }
+
+  function displayCurrentDay(data) {
+    const div = document.createElement("div");
+    const current = document.getElementById("current");
+    div.classList.add("forecasts");
+    let icon = getIcon(data.forecast.condition);
+    const degreesIcon = "&#176;";
+
+    div.innerHTML = `
+    <span class="condition symbol">${icon}</span>
+    <span class="condition">
+    <span class="forecast-data">${data.name}</span>
+    <span class="forecast-data">${data.forecast.low}${degreesIcon}/${data.forecast.high}${degreesIcon}</span>
+    <span class="forecast-data">${data.forecast.condition}</span>
+    </span>
+    `;
+    current.appendChild(div);
+  }
+  function displayThreeDay(data) {
+    const upcoming = document.getElementById("upcoming");
+    data.forecast.forEach((day) => {
+      const icon = getIcon(day.condition);
+      const degreesIcon = "&#176;";
+      const div = document.createElement("div");
+      div.classList.add("forecast-info");
+
+      div.innerHTML = `
+      <span class="upcoming">
+      <span class="symbol">${icon}</span>
+    <span class="forecast-data">${day.low}${degreesIcon}/${day.high}${degreesIcon}</span>
+    <span class="forecast-data">${day.condition}</span>
+    </span>
+    `;
+      upcoming.appendChild(div);
+    });
+  }
+  function getIcon(condition) {
+    switch (condition) {
+      case "Sunny":
+        return "&#x2600;";
+      case "Partly sunny":
+        return "&#x26C5;";
+      case "Overcast":
+        return "&#x2601;";
+      case "Rain":
+        return "&#x2614;";
+    }
+  }
 }
 
 attachEvents();
--- JS Advanced/JS Applications/Asynchronous Programming - Exercise/04.Locked-Profile/app.js
+++ JS Advanced/JS Applications/Asynchronous Programming - Exercise/04.Locked-Profile/app.js
@@ -1,3 +1,50 @@
 function lockedProfile() {
-    console.log('TODO...')
+  fetch("http://localhost:3030/jsonstore/advanced/profiles")
+    .then((res) => res.json())
+    .then((data) => displayProfiles(data))
+    .catch((err) => console.log(err));
+
+  function displayProfiles(data) {
+    document.getElementById("main").innerHTML = "";
+    Object.entries(data).forEach(([id, user]) => {
+      const div = document.createElement("div");
+      div.classList.add("profile");
+      div.innerHTML = `
+      <img src="./iconProfile2.png" class="userIcon" />
+				<label>Lock</label>
+				<input type="radio" name="user${id}Locked" value="lock" checked>
+				<label>Unlock</label>
+				<input type="radio" name="user${id}Locked" value="unlock"><br>
+				<hr>
+				<label>Username</label>
+				<input type="text" name="user1Username" value="${user.username}" disabled readonly />
+				<div id="user${id}HiddenFields">
+					<hr>
+					<label>Email:</label>
+					<input type="email" name="user${id}Email" value="${user.email}" disabled readonly />
+					<label>Age:</label>
+					<input type="email" name="user${id}Age" value="${user.age}" disabled readonly />
+				</div>
+				
+				<button>Show more</button>
+      `;
+      div.querySelector(`div`).style.display = "none";
+      document.getElementById("main").appendChild(div);
+    });
+
+    const profiles = document.querySelectorAll(".profile");
+    profiles.forEach((profile) => {
+      const button = profile.querySelector("button");
+      button.addEventListener("click", (e) => {
+        const locked = profile.querySelector('input[value="lock"]').checked;
+        if (locked === false && e.target.textContent == "Show more") {
+          e.target.textContent = "Hide it";
+          e.target.parentElement.querySelector("div").style.display = "block";
+        } else {
+          e.target.parentElement.querySelector("div").style.display = "none";
+          e.target.textContent = "Show more";
+        }
+      });
+    });
+  }
 }
